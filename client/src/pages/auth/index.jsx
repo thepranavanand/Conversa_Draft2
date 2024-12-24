14ed8b298e78ac4e467fed5ffeee0e1ba541cc5a
Pranav Anand <thepranavanand@gmail.com>
2024-12-25 03:18:15 +0530

    auth integration complete

--- client/src/pages/auth/index.jsx
+++ client/src/pages/auth/index.jsx
@@ -7,13 +7,26 @@ import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import { toast } from "sonner";
 import {apiClient} from "@/lib/api-client"
-import { SIGNUP_ROUTE } from "@/utils/constants";
+import { LOGIN_ROUTE, SIGNUP_ROUTE } from "@/utils/constants";
+import { useNavigate } from "react-router-dom";
 const Auth = () => {
 
-
+    const navigate = useNavigate() 
     const [email, setEmail] = useState("")
     const [password, setPassword] = useState("")
     const [confirmPassword, setConfirmPassword] = useState("")
+
+    const validateLogin = () => {
+        if(!email.length) {
+            toast.error("Email is required.");
+            return false;
+        }
+        if(!password.length){
+            toast.error("Password is required");
+            return false;
+        }
+        return true;
+    }
 
     const validateSignup = () => {
         if(!email.length) {
@@ -35,10 +48,22 @@ const Auth = () => {
         return true;
     }
 
-    const handleLogin = async() => {};
+    const handleLogin = async() => {
+        if(validateLogin()){
+            const response = await apiClient.post(LOGIN_ROUTE,{email, password}, {withCredentials: true});
+            console.log({response});
+            if(response.data.user.id){
+                if(response.data.user.profileSetup) navigate("/chat");
+                else navigate("/profile");
+            }
+        }
+    };
     const handleSignup = async() => {
         if(validateSignup()){
-            const response = await apiClient.post(SIGNUP_ROUTE,{email,password});
+            const response = await apiClient.post(SIGNUP_ROUTE,{email,password}, {withCredentials: true});
+            if(response.status===201){
+                navigate("/profile");
+            }
             console.log({response});
         }
     };
@@ -57,7 +82,7 @@ const Auth = () => {
                     </p>
                 </div>
                 <div className="flex items-center justify-center w-full">
-                    <Tabs className="w-3/4">
+                    <Tabs className="w-3/4" defaultValue="login">
                         <TabsList className="bg-transparent justify-center w-full">
                             <TabsTrigger value="login" className="data-[state=active]:bg-transparent text-black text-opacity-90 border-b-2 rounded-none w-full data-[state=active]:text-black data-[state=active]:font-semibold data-[state=active]:border-b-purple-500 p-3 transition-all duration-300">Login</TabsTrigger>
                             <TabsTrigger value="signup" className="data-[state=active]:bg-transparent text-black text-opacity-90 border-b-2 rounded-none w-full data-[state=active]:text-black data-[state=active]:font-semibold data-[state=active]:border-b-purple-500 p-3 transition-all duration-300">Signup</TabsTrigger>
--- server/controllers/AuthController.js
+++ server/controllers/AuthController.js
@@ -1,6 +1,8 @@
+import { compare } from "bcrypt";
 import User from "../models/UserModel.js";
 import jwt from "jsonwebtoken";
 const maxAge = 3*24*60*60*1000;
+
 
 const createToken = (email, userId) => {
     return jwt.sign({email,userId}, process.env.JWT_KEY, {expiresIn:maxAge});
@@ -22,6 +24,42 @@ export const signup = async (request, response, next)=>{
             id: user.id,
             email: user.email,
             profileSetup: user.profileSetup,
+        },
+    });
+    }catch(error){
+        console.log({error});
+        return response.status(500).send("Internal Server Error");
+    }
+
+}
+
+export const login = async (request, response, next)=>{
+    try{
+        const {email, password}= request.body;
+        if(!email || !password){
+            return response.status(400).send("Email and Password is required")
+        }
+        const user  = await User.findOne({email});
+        if(!user){
+            return response.status(404).send("User with given email not found.")
+        }
+        const auth = await compare(password, user.password);
+        if(!auth){
+            return response.status(400).send("Password is incorrect.")
+        }
+        response.cookie("jwt", createToken(email,user.id),{
+            maxAge,
+            secure:true,
+            sameSite:"None",
+        });
+        return response.status(200).json({user:{
+            id: user.id,
+            email: user.email,
+            profileSetup: user.profileSetup,
+            firstName: user.firstName,
+            lastName: user.lastName,
+            image: user.image,
+            color: user.color,
         },
     });
     }catch(error){
